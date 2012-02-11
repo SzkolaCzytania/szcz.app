b6f058571dc98b9e81b6dda85add66497013eff2
Andrew Mleczko <andrew@mleczko.net>
2012-02-11 17:06:53 +0100

    add resources

---
+++ src/szcz/resources.py
@@ -0,0 +1,21 @@
+from fanstatic import Library
+from fanstatic import Resource
+from fanstatic import Group
+from js.bootstrap import bootstrap_responsive_css, bootstrap_js
+
+library = Library('', 'resources')
+css_resource = Resource(library, 'main.css', depends=[bootstrap_responsive_css])
+js_resource = Resource(library, 'main.js', depends=[bootstrap_js])
+szcz = Group([css_resource, js_resource,])
+
+def pserve():
+    """A script aware of static resource"""
+    import pyramid.scripts.pserve
+    import pyramid_fanstatic
+    import os
+
+    dirname = os.path.dirname(__file__)
+    dirname = os.path.join(dirname, 'resources')
+    pyramid.scripts.pserve.add_file_callback(
+                pyramid_fanstatic.file_callback(dirname))
+    pyramid.scripts.pserve.main()
